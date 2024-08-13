5e1c78ab212a2bf676291fc825170d2c50b1cafa
mohadese <mohadeseh.bazii@gmail.com>
2024-08-13 15:44:06 +0330

    uploade in github

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-
 import time
 import tkinter as tk
+import string
 from tkinter import ttk, scrolledtext, messagebox
 
 
 def print_suggested_words(suggested_words, time_spent):
     result = ""
-    for key in list(suggested_words.keys()):
+    for key in suggested_words.keys():
         result += key + " :"
         for word in suggested_words[key]:
             result += word
@@ -40,8 +40,7 @@ def edit_distance(word, correct_word):
 
 def find_suggested_words(current_words_dict):
     suggested_words = {}
-    wrong_words_list = list(current_words_dict.keys())
-    for word in wrong_words_list:
+    for word in current_words_dict.keys():
         current_list = current_words_dict[word]
         suggested_dict = {}
         i = 1
@@ -65,8 +64,7 @@ def find_suggested_words(current_words_dict):
 
 def find_current_words(wrong_dict, path):
     current_words_dict = {}
-    wrong_words_list = list(wrong_dict.keys())
-    for word in wrong_words_list:
+    for word in wrong_dict.keys():
         current_words_dict[word] = []
         current_section_start = word[0: wrong_dict[word]]
         current_section_end = word[1:]
@@ -81,13 +79,11 @@ def find_current_words(wrong_dict, path):
 def find_wrong_word(text_list, tree):
     wrong_dict = {}
     dictionary = tree
-    text_list = list(set(text_list))
-    for word in text_list:
+    for word in set(text_list):
         i = 0
-        letter_list = list(word)
-        for index, letter in enumerate(letter_list):
-            if letter in list(dictionary.keys()):
-                if index == len(letter_list) - 1:
+        for index, letter in enumerate(word):
+            if letter in dictionary.keys():
+                if index == len(word) - 1:
                     if "$" in list(dictionary.keys()):
                         if letter not in dictionary["$"]:
                             wrong_dict[word] = i
@@ -104,7 +100,7 @@ def find_wrong_word(text_list, tree):
 def text_to_list(text):
     text = text.lower()
     word_list = []
-    punctuation = '''!()-[]{};:'"\\,<>./?@#$%^&*_~'''
+    punctuation = string.punctuation
     for word in text.split(" "):
         cleaned_word = ''.join(char for char in word if char not in punctuation)
         if cleaned_word.isalpha():
@@ -113,13 +109,12 @@ def text_to_list(text):
 
 
 def add_word(word, tree):
-    letter_list = list(word)
     current_dict = tree
-    for index, letter in enumerate(letter_list):
-        if letter not in list(current_dict.keys()):
+    for index, letter in enumerate(word):
+        if letter not in current_dict.keys():
             current_dict[letter] = {}
-        if index == len(letter_list) - 1:
-            if "$" in list(current_dict.keys()):
+        if index == len(word) - 1:
+            if "$" in current_dict.keys():
                 if letter not in current_dict["$"]:
                     current_dict["$"].append(letter)
             else:
@@ -140,74 +135,60 @@ class SpellCheckerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Spell Checker")
-        self.center = self.center_window(900, 600)
-        self.root.geometry(self.center)
+        self.root.geometry('600x500')
         self.language = tk.StringVar()
         self.text_input = tk.StringVar()
-        self.notebook = ttk.Notebook(self.root)
-        self.language_frame = ttk.Frame(self.notebook)
-        self.text_frame = ttk.Frame(self.notebook)
-        self.text_area = scrolledtext.ScrolledText(self.text_frame, wrap=tk.WORD, width=100, height=25)
         self.path = None
         self.create_main_window()
 
-    def center_window(self, width, height):
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
-        x = (screen_width // 2) - (width // 2)
-        y = (screen_height // 2) - (height // 2)
-        return f'{width}x{height}+{x}+{y}'
-
     def create_main_window(self):
-        self.notebook.pack(padx=10, pady=10)
-        self.notebook.add(self.language_frame, text='Language Selection')
-        self.notebook.add(self.text_frame, text='Text Input')
-        self.create_language_selection()
-        self.create_text_input()
-
-    def create_language_selection(self):
-        ttk.Label(self.language_frame, text="Select Language:").pack(pady=10)
-        language_combobox = ttk.Combobox(self.language_frame, textvariable=self.language, values=["English", "فارسی"])
-        language_combobox.pack(pady=5)
-        ttk.Button(self.language_frame, text="Next", command=self.set_language).pack(pady=20)
+        tk.Label(self.root, text="Select Language :").place(x=10, y=50)
+        language_combobox = ttk.Combobox(self.root, textvariable=self.language,
+                                         values=["English", "فارسی"], width=10, height=5)
+        language_combobox.place(x=130, y=53)
+        ttk.Label(self.root, text="Enter Text:").place(x=40, y=100)
+        self.text_input = tk.Text(self.root, width=60, height=17)
+        self.text_input.place(x=40 , y=130)
+        ttk.Button(self.root, text="Check Spelling", command=self.check_spelling).place(x=300, y=440)
+        ttk.Button(self.root, text='Exit', command=self.exit).place(x=100, y=440)
 
     def set_language(self):
-        if self.language.get() == "فارسی":
-            self.path = "dictionary_fa.txt"
+        if not self.language.get() :
+            messagebox.showwarning("Warning", "Please select a language")
+            return False
         else:
-            self.path = "dictionary.txt"
-        self.notebook.select(self.text_frame)
+            if self.language.get() == "فارسی":
+                self.path = "dictionary_fa.txt"
+            elif self.language.get() == "English":
+                self.path = "dictionary.txt"
+            return True
 
-    def create_text_input(self):
-        ttk.Label(self.text_frame, text="Enter Text:").pack(pady=10)
-        self.text_area.pack(pady=10)
-        ttk.Button(self.text_frame, text="Check Spelling", command=self.check_spelling).pack(pady=20)
+    def exit(self):
+        self.root.quit()
 
     def check_spelling(self):
-        paragraph = self.text_area.get("1.0", tk.END).strip()
-        if not paragraph:
-            messagebox.showwarning("Warning", "Please enter some text.")
-            return
+        if self.set_language():
+            paragraph = self.text_input.get('1.0', tk.END).strip()
+            if not paragraph:
+                messagebox.showwarning("Warning", "Please enter some text.")
+                return
 
-        start_time = time.time()
-        tree = upload_dictionary({}, self.path)
-        tex_list = text_to_list(paragraph)
-        wrong_dict = find_wrong_word(tex_list, tree)
-        current_words = find_current_words(wrong_dict, self.path)
-        suggested_words = find_suggested_words(current_words)
-        end_time = time.time()
-        time_spent = end_time - start_time
-        result = print_suggested_words(suggested_words, time_spent)
-        self.show_results(paragraph, wrong_dict, result)
+            start_time = time.time()
+            tree = upload_dictionary({}, self.path)
+            tex_list = text_to_list(paragraph)
+            wrong_dict = find_wrong_word(tex_list, tree)
+            current_words = find_current_words(wrong_dict, self.path)
+            suggested_words = find_suggested_words(current_words)
+            end_time = time.time()
+            time_spent = end_time - start_time
+            result = print_suggested_words(suggested_words, time_spent)
+            self.next()
+            self.show_results(paragraph, wrong_dict, result)
 
     def show_results(self, paragraph, wrong_words, result):
-        result_window = tk.Toplevel(self.root)
-        result_window.title("Results")
-        result_window.geometry(self.center)
-        frame = ttk.Frame(result_window)
-        frame.pack(pady=10, padx=10)
-        result_area = scrolledtext.ScrolledText(frame, wrap=tk.WORD, width=100, height=30)
-        result_area.pack(pady=10, padx=10)
+        self.root.config(bd=10)
+        result_area = scrolledtext.ScrolledText(self.root, wrap=tk.WORD, width=100, height=25)
+        result_area.pack(pady=20)
         result_area.tag_config('error', foreground='red')
         words = paragraph.split()
         for word in words:
@@ -218,6 +199,10 @@ class SpellCheckerApp:
         result_area.insert(tk.END, "\n\nSuggestions:\n")
         result_area.insert(tk.END, result)
         result_area.config(state=tk.DISABLED)
+
+    def next(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
 
 
 def main():
